be98a8c2c9dd42016cc4909726c0631515a571f2
gnoesnooj <306yyy@naver.com>
2026-04-05 00:23:05 +0900

    refactor: 잘못된 호출명 변경

--- src/main/java/com/followMe/order_server/order/infrastructure/client/delivery/DeliveryFeignClient.java
+++ src/main/java/com/followMe/order_server/order/infrastructure/client/delivery/DeliveryFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "delivery-service")
+@FeignClient(name = "delivery-server", fallbackFactory = DeliveryFeignClientFallbackFactory.class)
 public interface DeliveryFeignClient {
 
   @PostMapping("/internal/v1/deliveries")
