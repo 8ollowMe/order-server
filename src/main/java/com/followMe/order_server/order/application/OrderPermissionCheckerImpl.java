3303f64f4bb61d169214b256d022353386d996e7
gnoesnooj <306yyy@naver.com>
2026-04-06 11:16:15 +0900

    refactor: 주문 수정 권한 확인에 대한 공통 로직 분리 및 spotless 적용

--- src/main/java/com/followMe/order_server/order/application/OrderPermissionCheckerImpl.java
+++ src/main/java/com/followMe/order_server/order/application/OrderPermissionCheckerImpl.java
@@ -12,42 +12,20 @@ public class OrderPermissionCheckerImpl implements OrderPermissionChecker {
 
   @Override
   public void checkDeletePermission(UserContext userContext, Order order) {
-    switch (userContext.userRole()) {
-      case VENDOR, DELIVERY_MANAGER -> {
-        throw new BusinessException(CommonErrorCode.FORBIDDEN);
-      }
-      case HUB_MANAGER -> {
-        if (userContext.hubId() == order.getReceiverVendor().getHubId()
-            || userContext.hubId() == order.getRequestVendor().getHubId()) {
-
-        } else {
-          throw new BusinessException((CommonErrorCode.FORBIDDEN));
-        }
-      }
-      case MASTER -> {}
-    }
+    checkUpdate(userContext, order);
   }
 
   @Override
   public void checkUpdatePermission(UserContext userContext, Order order) {
-    switch (userContext.userRole()) {
-      case VENDOR, DELIVERY_MANAGER -> {
-        throw new BusinessException(CommonErrorCode.FORBIDDEN);
-      }
-      case HUB_MANAGER -> {
-        if (userContext.hubId() == order.getReceiverVendor().getHubId()
-            || userContext.hubId() == order.getRequestVendor().getHubId()) {
-
-        } else {
-          throw new BusinessException((CommonErrorCode.FORBIDDEN));
-        }
-      }
-      case MASTER -> {}
-    }
+    checkUpdate(userContext, order);
   }
 
   @Override
   public void checkCancelPermission(UserContext userContext, Order order) {
+    checkUpdate(userContext, order);
+  }
+
+  private void checkUpdate(UserContext userContext, Order order) {
     switch (userContext.userRole()) {
       case VENDOR, DELIVERY_MANAGER -> {
         throw new BusinessException(CommonErrorCode.FORBIDDEN);
